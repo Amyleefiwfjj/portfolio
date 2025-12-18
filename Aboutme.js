f2273f5c998fb4c976fbbb176c8e07128bbd80f0
Amyleefiwfjj <2804amylee@naver.com>
2025-12-18 21:50:40 +0900

    wowww

--- Aboutme.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-    // 로딩 화면 숨기기
-    const loadingScreen = document.getElementById('loading-screen');
-    const content = document.getElementById('content');
-
-    // 로딩 화면을 숨기고 콘텐츠를 표시
-    loadingScreen.style.display = 'none';
-    content.style.display = 'block';
-});
